0aed994833722f0c96c76d44e878880d188bda00
anoopezhap <anoopezhap@gmail.com>
2024-04-19 10:22:37 -0400

    All bugs fixed

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ import express from "express";
 
 import postRouter from "./routes/post.routes.js";
 import generateImageRouter from "./routes/generateImage.routes.js";
+import corsOptions from "./config/corsOptions.js";
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
